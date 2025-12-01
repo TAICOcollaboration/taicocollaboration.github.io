f9bc2e716798ce85b97fc5f57dcb31660bb28b71
kmurase <murase@psu.edu>
2025-12-01 15:10:50 +0900

    Add files via upload

---
+++ assets/entries/pages_participants.C9WbGnMJ.js
@@ -0,0 +1,167 @@
+import{j as t,l as p,m as f,y as x,C as j,e as u,A as c,I as l,g as i,R as w,a as d,i as o,d as y}from"../chunks/chunk-D5Fhv-xz.js";import{P as k}from"../chunks/chunk-DGFMXN4i.js";/* empty css                      */const b="taicocollaboration",z=[{id:"Ddy2Gh07RZ2CGpUn1Z_k4"},{id:"CE_Wk48-Irzg84HFQqWSC",maxWidth:991},{id:"RoZ1EB0pqazJC5Qo1Svoo",maxWidth:767},{id:"rYrmYXU4qD8xYaNG5oeZW",maxWidth:479}],v="image1_y7Y8Y5iQEgd-9Si08s7uZ.png",N=["ZenKakuGothicNew-Light_6e8A9E5YGQsyVVwNThwCe.ttf","ZenKakuGothicNew-Black_qy6AunPeKAlG88OwJRn9b.ttf","KumbhSans-VariableFont_YOPQ_wght_yVnpS2yF-XrAj9iEAURzD.ttf","ZenKakuGothicNew-Medium_c1jx8zN0SD3qqTcs2ca7b.ttf","ZenKakuGothicNew-Bold_GnGbIITN7bOHbJDn_EO79.ttf","ZenKakuGothicNew-Regular_m8gOZ5m158Kmlg-4ZLuCI.ttf"],A=["taicobanner_tkmmx7OY1QBrYTGCaTCVF.jpeg"],q=n=>t.jsx("body",{className:"w-element",children:t.jsxs("div",{className:"w-element c1m0hj8x cj9pjua czti6bo cnr2a0m",children:[t.jsx(p,{children:t.jsx(f,{children:t.jsxs("div",{className:"w-element c5cic0a c168co9w ccvf28h csluvar cmodvuy c1gd8pjp c3n4l6m czer21n",children:[t.jsx(x,{src:"/assets/justtaico_P7rIgCjlhGh30wH9Fkckp.png",width:120,height:322,alt:"",className:"w-image c1yy71cz c3n4l6m crtqnol cnn03j5 c1lm6uqj c1bogr21 c1e3w95h"}),t.jsx("p",{className:"w-element c3yfew2 c1rjzd65 c3n4l6m ctk8am2",children:"Tracking Aincient Imprints of Cosmic Origins"}),t.jsx("h1",{className:"w-element c3yfew2 csz0rx4 c3n4l6m c1k45by1 c2v6zxd c1ad00ct",children:"第１回TAICOワークショップ"}),t.jsx(j,{className:"w-navigation-menu cc524ox crw6394 c3yfew2 c8xivqd c1oz29ph ccvf28h csluvar czvqdnq c1gd8pjp c11n1060 c7asojn c1ivrync cjnzvd0",children:t.jsxs(u,{className:"w-menu-list ctxnml c10p7a2c c1wbq9ys cxw5dtq cplpwym c1mpx3ta c19nybdz c1e292zd cyugzxw c3yfew2 c28vjdq cpxk0cr c1ivrync cjnzvd0 cuhlv3 c4vby65 c3n5m5x",children:[t.jsx(c,{"data-ws-index":"0",className:"w-menu-item",children:t.jsx(l,{children:t.jsx(i,{href:"/",className:"w-link c1er32pt c1mpx3ta c19nybdz c41qek1 ci32gnp c6vwt7v c1pzsfjl c1277m01 c1pf2qzj c19juk8b c623wjz c1894r19 ctk8am2 c1yd6k3e c1raman9 c13uynxa c10jimfk clprrcx c11zg9tr ca0o4lw ce90ew9 c1ckm4nz c1ka6ndf c15qz4op chrbgp4 c1ghelss cofc9a8 c1ofb0pf c1ypgk86 cfu09eh c1du6td4 c1mfxfe cg8tsxv c1a0wloe",children:"Home"})})}),t.jsx(c,{"data-ws-index":"1",className:"w-menu-item",children:t.jsx(l,{children:t.jsx(i,{href:"/registration",className:"w-link c1er32pt c1mpx3ta c19nybdz c41qek1 ci32gnp c6vwt7v c1pzsfjl c1277m01 c1pf2qzj c19juk8b c623wjz c1894r19 ctk8am2 c1yd6k3e c1raman9 c13uynxa c10jimfk clprrcx c11zg9tr ca0o4lw ce90ew9 c1ckm4nz c1ka6ndf c15qz4op chrbgp4 c1ghelss cofc9a8 c1ofb0pf c1ypgk86 cfu09eh c1du6td4 c1mfxfe cg8tsxv c1a0wloe",children:"Registration"})})}),t.jsx(c,{"data-ws-index":"2",className:"w-menu-item",children:t.jsx(l,{children:t.jsx(i,{href:"/program",className:"w-link c1er32pt c1mpx3ta c19nybdz c41qek1 ci32gnp c6vwt7v c1pzsfjl c1277m01 c1pf2qzj c19juk8b c623wjz c1894r19 ctk8am2 c1yd6k3e c1raman9 c13uynxa c10jimfk clprrcx c11zg9tr ca0o4lw ce90ew9 c1ckm4nz c1ka6ndf c15qz4op chrbgp4 c1ghelss cofc9a8 c1ofb0pf c1ypgk86 cfu09eh c1du6td4 c1mfxfe cg8tsxv c1a0wloe",children:"Program"})})}),t.jsx(c,{"data-ws-index":"3",className:"w-menu-item",children:t.jsx(l,{children:t.jsx(i,{href:"/participants",className:"w-link c1er32pt c1mpx3ta c19nybdz c41qek1 ci32gnp c6vwt7v c1pzsfjl c1277m01 c1pf2qzj c19juk8b c623wjz c1894r19 ctk8am2 c1yd6k3e c1raman9 c13uynxa c10jimfk clprrcx c11zg9tr ca0o4lw ce90ew9 c1ckm4nz c1ka6ndf c15qz4op chrbgp4 c1ghelss cofc9a8 c1ofb0pf c1ypgk86 cfu09eh c1du6td4 c1mfxfe cg8tsxv c1a0wloe",children:"Participants"})})}),t.jsx(c,{"data-ws-index":"4",className:"w-menu-item",children:t.jsx(l,{children:t.jsx(i,{href:"/information",className:"w-link c1er32pt c1mpx3ta c19nybdz c41qek1 ci32gnp c6vwt7v c1pzsfjl c1277m01 c1pf2qzj c19juk8b c623wjz c1894r19 ctk8am2 c1yd6k3e c1raman9 c13uynxa c10jimfk clprrcx c11zg9tr ca0o4lw ce90ew9 c1ckm4nz c1ka6ndf c15qz4op chrbgp4 c1ghelss cofc9a8 c1ofb0pf c1ypgk86 cfu09eh c1du6td4 c1mfxfe cg8tsxv c1a0wloe",children:"Information"})})})]})})]})})}),t.jsx("div",{className:"w-element cj9pjua c1yy71cz c1tbsvft c3n5e8s",children:t.jsxs("div",{className:"w-element",children:[t.jsx("h1",{className:"w-element",children:"招待講演者"}),t.jsx(k,{code:`<style>
+  .participant-table {
+    width: 100%;
+    border-collapse: collapse;
+    font-family: Zen Kaku Gothic New; /* replace with your font */
+    font-size: 15px;
+  }
+
+  .participant-table th,
+  .participant-table td {
+    padding: 10px 14px;
+    border: 1px solid #ddd;
+    text-align: left;
+  }
+
+  .participant-table th {
+    background-color: #f1f1f1;
+    font-weight: 600;
+  }
+
+  /* Mobile responsive */
+  @media screen and (max-width: 600px) {
+    .participant-table thead {
+      display: none;
+    }
+    .participant-table tr {
+      display: block;
+      margin-bottom: 12px;
+      padding: 12px;
+      border: 1px solid #ccc;
+      border-radius: 6px;
+    }
+    .participant-table td {
+      display: flex;
+      justify-content: space-between;
+      padding: 6px 4px;
+      border: none;
+      border-bottom: 1px solid #eee;
+    }
+    .participant-table td:last-child {
+      border-bottom: none;
+    }
+    .participant-table td::before {
+      content: attr(data-label);
+      font-weight: bold;
+      color: #333;
+      margin-right: 8px;
+    }
+  }
+</style>
+
+<table class="participant-table">
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Affiliation</th>
+    </tr>
+  </thead>
+  <tbody>
+
+    <tr>
+      <td data-label="Name">芦田洋輔</td>
+      <td data-label="Affiliation">東北大学</td>
+    </tr>    
+    <tr>
+      <td data-label="Name">阿部なつ江</td>
+      <td data-label="Affiliation">JAMSTEC</td>
+    </tr>
+    <tr>
+      <td data-label="Name">石川法人</td>
+      <td data-label="Affiliation">JAEA</td>
+    </tr>
+
+    <tr>
+      <td data-label="Name">殷文</td>
+      <td data-label="Affiliation">東京都立大学</td>
+    </tr>
+
+    <tr>
+      <td data-label="Name">大平豊</td>
+      <td data-label="Affiliation">東京大学</td>
+    </tr>
+    
+    <tr>
+      <td data-label="Name">日高洋</td>
+      <td data-label="Affiliation">名古屋大学</td>
+    </tr>
+    <tr>
+      <td data-label="Name">齊藤天晴</td>
+      <td data-label="Affiliation">JAEA</td>
+    </tr>
+    <tr>
+      <td data-label="Name">岡崎隆司</td>
+      <td data-label="Affiliation">九州大学</td>
+    </tr>
+
+    <tr>
+      <td data-label="Name">加藤ちなみ</td>
+      <td data-label="Affiliation">東京大学</td>
+    </tr>
+
+    <tr>
+      <td data-label="Name">中竜大</td>
+      <td data-label="Affiliation">東邦大学</td>
+    </tr>
+    <tr>
+      <td data-label="Name">行方宏介</td>
+      <td data-label="Affiliation">京都大学</td>
+    </tr>
+    <tr>
+      <td data-label="Name">仁木創太</td>
+      <td data-label="Affiliation">名古屋大学</td>
+    </tr>
+    
+    <tr>
+      <td data-label="Name">長谷部徳子</td>
+      <td data-label="Affiliation">金沢大学</td>
+    </tr>
+
+    <tr>
+      <td data-label="Name">藤井悠里</td>
+      <td data-label="Affiliation">京都大学</td>
+    </tr>
+    
+    <tr>
+      <td data-label="Name">松本徹</td>
+      <td data-label="Affiliation">京都大学</td>
+    </tr>
+    <tr>
+      <td data-label="Name">道林克禎</td>
+      <td data-label="Affiliation">名古屋大学</td>
+    </tr>    
+
+    <tr>
+      <td data-label="Name">望月優子</td>
+      <td data-label="Affiliation">理化学研究所</td>
+    </tr>
+
+
+    <tr>
+      <td data-label="Name">森脇可奈</td>
+      <td data-label="Affiliation">東京大学</td>
+    </tr>
+    <tr>
+      <td data-label="Name">渡邊誠一郎</td>
+      <td data-label="Affiliation">名古屋大学</td>
+    </tr>
+    <tr>
+      <td data-label="Name">湯口貴史</td>
+      <td data-label="Affiliation">熊本大学</td>
+    </tr>    
+    <tr>
+      <td data-label="Name">吉田純也</td>
+      <td data-label="Affiliation">東北大学</td>
+    </tr>  
+    
+    <tr>
+      <td data-label="Name">和南城伸也 </td>
+      <td data-label="Affiliation">京都大学</td>
+    </tr>
+
+
+  
+    
+  </tbody>
+</table>
+`,className:"w-html-embed"})]})}),t.jsx(p,{children:t.jsx(f,{children:t.jsx("footer",{className:"w-element cvap6db c5cic0a c168co9w c1df4140 csluvar czvqdnq c1v736zy clz7zg9 c13lh67r cj9pjua c1xtr0q c1m0hj8x cpnjnz3 ctxnml c1s88ok0 c1mpx3ta c19nybdz c1nwaejy c1yy71cz clouc1b c13eprvs co6u0op c1k6nk8b c1614ayy c1kgcc8m c15nbcuf c1ko7c42 c1u5rs2g c12hoc41 c7asojn c1qapcvi",children:t.jsx(x,{src:"/assets/image1_y7Y8Y5iQEgd-9Si08s7uZ.png",width:80,height:80,alt:"",className:"w-image crw6394 crtqnol c1m2e552 carbcwc c12jp8jn"})})})})]})}),S=({data:n})=>{const{system:e,resources:r,url:s}=n;return t.jsx(w.Provider,{value:{imageLoader:o,assetBaseUrl:d,resources:r,breakpoints:z,onError:console.error},children:t.jsx(q,{system:e},s)})},_=Object.freeze(Object.defineProperty({__proto__:null,default:S},Symbol.toStringTag,{value:"Module"})),T=({data:n})=>{const{pageMeta:e}=n,{origin:r}=new URL(n.url),s={"@context":"https://schema.org","@type":"WebSite",name:b,url:r};let m=e.socialImageUrl;e.socialImageAssetName&&(m=`${r}${o({src:`${d}/${e.socialImageAssetName}`})}`);const h=e.custom.some(a=>a.property==="twitter:card");return t.jsxs(t.Fragment,{children:[n.url&&t.jsx("meta",{property:"og:url",content:n.url}),t.jsx("title",{children:e.title}),t.jsx("meta",{property:"og:title",content:e.title}),e.description&&t.jsxs(t.Fragment,{children:[t.jsx("meta",{name:"description",content:e.description}),t.jsx("meta",{property:"og:description",content:e.description})]}),t.jsx("meta",{property:"og:type",content:"website"}),t.jsx("meta",{property:"og:site_name",content:b}),m&&t.jsx("meta",{property:"og:image",content:e.socialImageUrl}),t.jsx("script",{type:"application/ld+json",dangerouslySetInnerHTML:{__html:JSON.stringify(s,null,2)}}),e.excludePageFromSearch&&t.jsx("meta",{name:"robots",content:"noindex, nofollow"}),e.custom.map(({property:a,content:g})=>t.jsx("meta",{property:a,content:g},a)),(e.socialImageAssetName!==void 0||e.socialImageUrl!==void 0)&&h===!1&&t.jsx("meta",{property:"twitter:card",content:"summary_large_image"}),t.jsx("link",{rel:"icon",href:o({src:`${d}${v}`})}),N.map(a=>t.jsx("link",{rel:"preload",href:`${d}${a}`,as:"font",crossOrigin:"anonymous"},a)),A.map(a=>t.jsx("link",{rel:"preload",href:`${d}${a}`,as:"image"},a))]})},P=Object.freeze(Object.defineProperty({__proto__:null,Head:T},Symbol.toStringTag,{value:"Module"})),G={isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:{server:!0}}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"/renderer/+onRenderClient.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:y}},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/participants/+Page.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:_}},Head:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/participants/+Head.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:P}}};export{G as configValuesSerialized};
---
+++ assets/entries/pages_program.CFVK2DDk.js
@@ -0,0 +1,349 @@
+import{j as e,l as m,m as b,y as f,C as u,e as w,A as c,I as d,g as l,R as k,a as r,i,d as y}from"../chunks/chunk-D5Fhv-xz.js";import{P as x}from"../chunks/chunk-DGFMXN4i.js";/* empty css                      */const h="taicocollaboration",S=[{id:"Ddy2Gh07RZ2CGpUn1Z_k4"},{id:"CE_Wk48-Irzg84HFQqWSC",maxWidth:991},{id:"RoZ1EB0pqazJC5Qo1Svoo",maxWidth:767},{id:"rYrmYXU4qD8xYaNG5oeZW",maxWidth:479}],z="image1_y7Y8Y5iQEgd-9Si08s7uZ.png",v=["ZenKakuGothicNew-Light_6e8A9E5YGQsyVVwNThwCe.ttf","ZenKakuGothicNew-Black_qy6AunPeKAlG88OwJRn9b.ttf","KumbhSans-VariableFont_YOPQ_wght_yVnpS2yF-XrAj9iEAURzD.ttf","ZenKakuGothicNew-Medium_c1jx8zN0SD3qqTcs2ca7b.ttf","ZenKakuGothicNew-Bold_GnGbIITN7bOHbJDn_EO79.ttf","ZenKakuGothicNew-Regular_m8gOZ5m158Kmlg-4ZLuCI.ttf"],T=["taicobanner_tkmmx7OY1QBrYTGCaTCVF.jpeg"],N=a=>e.jsx("body",{className:"w-element",children:e.jsxs("div",{className:"w-element c1m0hj8x cj9pjua czti6bo cnr2a0m",children:[e.jsx(m,{children:e.jsx(b,{children:e.jsxs("div",{className:"w-element c5cic0a c168co9w ccvf28h csluvar cmodvuy c1gd8pjp c3n4l6m czer21n",children:[e.jsx(f,{src:"/assets/justtaico_P7rIgCjlhGh30wH9Fkckp.png",width:120,height:322,alt:"",className:"w-image c1yy71cz c3n4l6m crtqnol cnn03j5 c1lm6uqj c1bogr21 c1e3w95h"}),e.jsx("p",{className:"w-element c3yfew2 c1rjzd65 c3n4l6m ctk8am2",children:"Tracking Aincient Imprints of Cosmic Origins"}),e.jsx("h1",{className:"w-element c3yfew2 csz0rx4 c3n4l6m c1k45by1 c2v6zxd c1ad00ct",children:"第１回TAICOワークショップ"}),e.jsx(u,{className:"w-navigation-menu cc524ox crw6394 c3yfew2 c8xivqd c1oz29ph ccvf28h csluvar czvqdnq c1gd8pjp c11n1060 c7asojn c1ivrync cjnzvd0",children:e.jsxs(w,{className:"w-menu-list ctxnml c10p7a2c c1wbq9ys cxw5dtq cplpwym c1mpx3ta c19nybdz c1e292zd cyugzxw c3yfew2 c28vjdq cpxk0cr c1ivrync cjnzvd0 cuhlv3 c4vby65 c3n5m5x",children:[e.jsx(c,{"data-ws-index":"0",className:"w-menu-item",children:e.jsx(d,{children:e.jsx(l,{href:"/",className:"w-link c1er32pt c1mpx3ta c19nybdz c41qek1 ci32gnp c6vwt7v c1pzsfjl c1277m01 c1pf2qzj c19juk8b c623wjz c1894r19 ctk8am2 c1yd6k3e c1raman9 c13uynxa c10jimfk clprrcx c11zg9tr ca0o4lw ce90ew9 c1ckm4nz c1ka6ndf c15qz4op chrbgp4 c1ghelss cofc9a8 c1ofb0pf c1ypgk86 cfu09eh c1du6td4 c1mfxfe cg8tsxv c1a0wloe",children:"Home"})})}),e.jsx(c,{"data-ws-index":"1",className:"w-menu-item",children:e.jsx(d,{children:e.jsx(l,{href:"/registration",className:"w-link c1er32pt c1mpx3ta c19nybdz c41qek1 ci32gnp c6vwt7v c1pzsfjl c1277m01 c1pf2qzj c19juk8b c623wjz c1894r19 ctk8am2 c1yd6k3e c1raman9 c13uynxa c10jimfk clprrcx c11zg9tr ca0o4lw ce90ew9 c1ckm4nz c1ka6ndf c15qz4op chrbgp4 c1ghelss cofc9a8 c1ofb0pf c1ypgk86 cfu09eh c1du6td4 c1mfxfe cg8tsxv c1a0wloe",children:"Registration"})})}),e.jsx(c,{"data-ws-index":"2",className:"w-menu-item",children:e.jsx(d,{children:e.jsx(l,{href:"/program",className:"w-link c1er32pt c1mpx3ta c19nybdz c41qek1 ci32gnp c6vwt7v c1pzsfjl c1277m01 c1pf2qzj c19juk8b c623wjz c1894r19 ctk8am2 c1yd6k3e c1raman9 c13uynxa c10jimfk clprrcx c11zg9tr ca0o4lw ce90ew9 c1ckm4nz c1ka6ndf c15qz4op chrbgp4 c1ghelss cofc9a8 c1ofb0pf c1ypgk86 cfu09eh c1du6td4 c1mfxfe cg8tsxv c1a0wloe",children:"Program"})})}),e.jsx(c,{"data-ws-index":"3",className:"w-menu-item",children:e.jsx(d,{children:e.jsx(l,{href:"/participants",className:"w-link c1er32pt c1mpx3ta c19nybdz c41qek1 ci32gnp c6vwt7v c1pzsfjl c1277m01 c1pf2qzj c19juk8b c623wjz c1894r19 ctk8am2 c1yd6k3e c1raman9 c13uynxa c10jimfk clprrcx c11zg9tr ca0o4lw ce90ew9 c1ckm4nz c1ka6ndf c15qz4op chrbgp4 c1ghelss cofc9a8 c1ofb0pf c1ypgk86 cfu09eh c1du6td4 c1mfxfe cg8tsxv c1a0wloe",children:"Participants"})})}),e.jsx(c,{"data-ws-index":"4",className:"w-menu-item",children:e.jsx(d,{children:e.jsx(l,{href:"/information",className:"w-link c1er32pt c1mpx3ta c19nybdz c41qek1 ci32gnp c6vwt7v c1pzsfjl c1277m01 c1pf2qzj c19juk8b c623wjz c1894r19 ctk8am2 c1yd6k3e c1raman9 c13uynxa c10jimfk clprrcx c11zg9tr ca0o4lw ce90ew9 c1ckm4nz c1ka6ndf c15qz4op chrbgp4 c1ghelss cofc9a8 c1ofb0pf c1ypgk86 cfu09eh c1du6td4 c1mfxfe cg8tsxv c1a0wloe",children:"Information"})})})]})})]})})}),e.jsxs("div",{className:"w-element cj9pjua c1yy71cz cwx26b3 c3n5e8s",children:[e.jsxs("h1",{className:"w-element csz0rx4",children:["プログラム","",e.jsx("br",{}),""]}),e.jsx(x,{code:`<style>
+  .conf-table {
+    width: 100%;
+    border-collapse: collapse;
+    font-family: Zen Kaku Gothic New;
+    font-size: 15px;
+  }
+
+  .conf-table th,
+  .conf-table td {
+    padding: 10px 14px;
+    border: 1px solid #ddd;
+    text-align: left;
+  }
+
+  .conf-table th {
+    background-color: #f1f1f1;
+  }
+
+  .conf-session {
+    font-weight: bold;
+    background-color: #fafafa;
+  }
+
+  /* Mobile responsive */
+  @media screen and (max-width: 600px) {
+    .conf-table thead {
+      display: none;
+    }
+    .conf-table tr {
+      display: block;
+      margin-bottom: 12px;
+      border: 1px solid #ccc;
+      padding: 10px;
+      border-radius: 6px;
+    }
+    .conf-table td {
+      display: flex;
+      justify-content: space-between;
+      padding: 8px 6px;
+      border: none;
+      border-bottom: 1px solid #eee;
+    }
+    .conf-table td:last-child {
+      border-bottom: none;
+    }
+    .conf-table td::before {
+      content: attr(data-label);
+      font-weight: bold;
+      color: #333;
+      margin-right: 8px;
+    }
+  }
+</style>
+
+<table class="conf-table">
+  <thead>
+    <tr>
+      
+    </tr>
+  </thead>
+  <tbody>
+    <tr class="conf-session">
+      <td colspan="4">12/25　午前 　座長-村瀬 </td>
+    </tr>
+
+    <tr>
+      <td data-label="Time">09:00–09:15</td>
+      <td data-label="Session">イントロダクション</td>
+      <td data-label="Speaker">村瀬孔大(PSU/YITP)</td>
+    </tr>
+
+    <tr>
+      <td data-label="Time">09:15–09:45</td>
+      <td data-label="Session">銀河宇宙線の起源と銀河の歴史</td>
+
+      <td data-label="Speaker">大平豊(東京大)</td>
+    </tr>
+
+    <tr>
+      <td data-label="Time">09:45–10:15</td>
+      <td data-label="Session">重元素の起源と銀河の歴史</td>
+      <td data-label="Speaker">和南城伸也(京都大基研)</td>
+
+    </tr>
+
+    <tr>
+      <td data-label="Time">10:15–10:45</td>
+      <td data-label="Session">超新星ニュートリノと銀河の歴史</td>
+      <td data-label="Speaker">加藤ちなみ(東京大)</td>
+
+    </tr>
+
+    <tr>
+      <td data-label="Time">10:45–11:00</td>
+      <td data-label="Session">コーヒー</td>
+
+    </tr>
+
+    <tr>
+      <td data-label="Time">11:00–11:15</td>
+      <td data-label="Session">太陽系誕生を探る小天体探査</td>
+      <td data-label="Speaker">渡邊誠一郎(名古屋大)</td>
+
+    </tr>
+
+    <tr>
+      <td data-label="Time">11:15–11:45</td>
+      <td data-label="Session">惑星系円盤への宇宙線への影響</td>
+      <td data-label="Speaker">藤井悠里(京都大)</td>
+
+    </tr>
+
+    <tr>
+      <td data-label="Time">11:45–12:15</td>
+      <td data-label="Session">若い太陽型の恒星の観測から探る太陽系初期の高エネルギー環境</td>
+      <td data-label="Speaker">行方宏介(京都大)</td>
+
+    </tr>
+
+    <tr class="conf-session">
+      <td colspan="4">12/25　午後　座長-橋口</td>
+    </tr>
+
+    <tr>
+      <td data-label="Time">13:45–14:15</td>
+      <td data-label="Session">宇宙線と惑星物質の相互作用：各種隕石中の核反応とトラック損傷の蓄積</td>
+      <td data-label="Speaker">日高洋(名古屋大)</td>
+
+    </tr>
+
+    <tr>
+      <td data-label="Time">14:15–14:45</td>
+      <td data-label="Session">希土類元素同位体変動を利用した中性子スペクトル解析による隕石・月の石が経験した惑星表層古環境の推定</td>
+      <td data-label="Speaker">齊藤天晴(JAEA)</td>
+
+    </tr>
+
+    <tr>
+      <td data-label="Time">14:45–15:15</td>
+      <td data-label="Session">貴ガス同位体による宇宙物質の高エネルギー粒子照射の履歴</td>
+      <td data-label="Speaker">岡崎隆司(九州大)</td>
+
+    </tr>
+
+    <tr>
+      <td data-label="Time">15:15–15:30</td>
+      <td data-label="Session">コーヒー</td>
+
+    </tr>
+
+    <tr>
+      <td data-label="Time">15:30–16:00</td>
+      <td data-label="Session">電子顕微鏡で見る地球外試料表面のエネルギー粒子照射の痕跡</td>
+      <td data-label="Speaker">松本徹(京都大)</td>
+
+    </tr>
+
+    <tr>
+      <td data-label="Time">16:00–16:30</td>
+      <td data-label="Session">アイスコアを用いて過去の超新星痕跡や太陽活動と気候変動との関係を探る：Astro-Glaciology Ice-core Data Factory Project</td>
+      <td data-label="Speaker">望月優子(RIKEN)</td>
+
+    </tr>
+
+    <tr>
+      <td data-label="Time">16:30–17:00</td>
+      <td data-label="Session">宇宙線によるミューオン破砕反応</td>
+      <td data-label="Speaker">芦田洋輔(東北大)</td>
+
+    </tr>
+
+    <tr>
+      <td data-label="Time">17:00–17:30</td>
+      <td data-label="Session">議論</td>
+      <td data-label="Speaker">橋口 &amp; 加藤</td>
+
+    </tr>
+
+    <tr>
+      <td data-label="Time">18:00–20:00</td>
+      <td data-label="Session">懇親会</td>
+            <td data-label="Speaker"></td>
+
+
+    </tr>
+  </tbody>
+</table>
+`,className:"w-html-embed csz0rx4"}),e.jsx("hr",{className:"w-element c3yfew2 c9f1l1m c1phncp7 c1welyf6 c1lniwab chbb0lk cl97uf"}),e.jsx(x,{code:`<style>
+  .conf-table {
+    width: 100%;
+    border-collapse: collapse;
+    font-family: Zen Kaku Gothic New;
+    font-size: 15px;
+  }
+
+  .conf-table th,
+  .conf-table td {
+    padding: 10px 14px;
+    border: 1px solid #ddd;
+    text-align: left;
+  }
+
+  .conf-table th {
+    background-color: #f1f1f1;
+  }
+
+  .conf-session {
+    font-weight: bold;
+    background-color: #fafafa;
+  }
+
+  /* Mobile responsive */
+  @media screen and (max-width: 600px) {
+    .conf-table thead {
+      display: none;
+    }
+    .conf-table tr {
+      display: block;
+      margin-bottom: 12px;
+      border: 1px solid #ccc;
+      padding: 10px;
+      border-radius: 6px;
+    }
+    .conf-table td {
+      display: flex;
+      justify-content: space-between;
+      padding: 8px 6px;
+      border: none;
+      border-bottom: 1px solid #eee;
+    }
+    .conf-table td:last-child {
+      border-bottom: none;
+    }
+    .conf-table td::before {
+      content: attr(data-label);
+      font-weight: bold;
+      color: #333;
+      margin-right: 8px;
+    }
+  }
+</style>
+
+<table class="conf-table">
+  <thead>
+    <tr>
+      
+    </tr>
+  </thead>
+  <tbody>
+    <tr class="conf-session">
+      <td colspan="4">12/26　午前 　座長-中 </td>
+    </tr>
+
+    <tr>
+      <td data-label="Time">09:00–09:30</td>
+      <td data-label="Session">ダークマターと宇宙の歴史</td>
+      <td data-label="Speaker">殷文(東京都立大)</td>
+    </tr>
+
+    <tr>
+      <td data-label="Time">09:30–10:00</td>
+      <td data-label="Session">太古検出器における粒子トラックのレビュー</td>
+
+      <td data-label="Speaker">中竜大(東邦大)</td>
+    </tr>
+
+    <tr>
+      <td data-label="Time">10:00–10:30</td>
+      <td data-label="Session">イオントラックについて（オンライン）</td>
+      <td data-label="Speaker">石川法人（JAEA）</td>
+
+    </tr>
+
+    <tr>
+      <td data-label="Time">10:30–10:45</td>
+      <td data-label="Session">コーヒー</td>
+
+    </tr>
+
+    <tr>
+      <td data-label="Time">10:45–11:15</td>
+      <td data-label="Session">放射光X線計測による太古検出器の解析手法の検討</td>
+      <td data-label="Speaker">吉田純也(東北大)</td>
+    </tr>
+
+    <tr>
+      <td data-label="Time">11:15–11:45</td>
+      <td data-label="Session">白雲母で天然に見られる粒子トラックについて（オンライン）</td>
+      <td data-label="Speaker">長谷部徳子（金沢大）</td>
+
+    </tr>
+
+    <tr>
+      <td data-label="Time">11:45–12:15</td>
+      <td data-label="Session">飛跡検出器の候補鉱物に含まれる放射性元素の濃度</td>
+      <td data-label="Speaker">仁木創太(名古屋大)</td>
+
+    </tr>
+
+
+    <tr class="conf-session">
+      <td colspan="4">12/26　午後　座長-加藤</td>
+    </tr>
+
+    <tr>
+      <td data-label="Time">13:45–14:15</td>
+      <td data-label="Session">太古検出器に適した天然鉱物の地質背景と産状条件（仮）(オンライン）</td>
+      <td data-label="Speaker">阿部なつ江(JAMSTEC）</td>
+
+    </tr>
+
+    <tr>
+      <td data-label="Time">14:15–14:45</td>
+      <td data-label="Session">超深海底の海溝かんらん岩の特徴と採取法</td>
+      <td data-label="Speaker">道林克禎(名古屋大)</td>
+
+    </tr>
+
+    <tr>
+      <td data-label="Time">14:45–15:15</td>
+      <td data-label="Session">花崗岩体の熱史と割れ目分布の関係：岩石学・熱年代学に基づいたアプローチ（オンライン）</td>
+      <td data-label="Speaker">湯口貴史（熊本大）</td>
+
+    </tr>
+
+    <tr>
+      <td data-label="Time">15:15–15:45</td>
+      <td data-label="Session">機械学習とその応用（オンライン）</td>
+      <td data-label="Speaker">森脇可奈（東京大）</td>
+
+    </tr>
+
+    <tr>
+      <td data-label="Time">15:45–16:00</td>
+      <td data-label="Session">コーヒー</td>
+
+    </tr>
+
+    <tr>
+      <td data-label="Time">16:00–17:00</td>
+      <td data-label="Session">まとめと議論</td>
+      <td data-label="Speaker">廣瀬 &amp; 中 &amp; 村瀬</td>
+
+    </tr>
+
+  </tbody>
+</table>
+`,className:"w-html-embed"})]}),e.jsx("p",{className:"w-element"}),e.jsx(m,{children:e.jsx(b,{children:e.jsx("footer",{className:"w-element cvap6db c5cic0a c168co9w c1df4140 csluvar czvqdnq c1v736zy clz7zg9 c13lh67r cj9pjua c1xtr0q c1m0hj8x cpnjnz3 ctxnml c1s88ok0 c1mpx3ta c19nybdz c1nwaejy c1yy71cz clouc1b c13eprvs co6u0op c1k6nk8b c1614ayy c1kgcc8m c15nbcuf c1ko7c42 c1u5rs2g c12hoc41 c7asojn c1qapcvi",children:e.jsx(f,{src:"/assets/image1_y7Y8Y5iQEgd-9Si08s7uZ.png",width:80,height:80,alt:"",className:"w-image crw6394 crtqnol c1m2e552 carbcwc c12jp8jn"})})})})]})}),q=({data:a})=>{const{system:t,resources:s,url:o}=a;return e.jsx(k.Provider,{value:{imageLoader:i,assetBaseUrl:r,resources:s,breakpoints:S,onError:console.error},children:e.jsx(N,{system:t},o)})},_=Object.freeze(Object.defineProperty({__proto__:null,default:q},Symbol.toStringTag,{value:"Module"})),A=({data:a})=>{const{pageMeta:t}=a,{origin:s}=new URL(a.url),o={"@context":"https://schema.org","@type":"WebSite",name:h,url:s};let p=t.socialImageUrl;t.socialImageAssetName&&(p=`${s}${i({src:`${r}/${t.socialImageAssetName}`})}`);const g=t.custom.some(n=>n.property==="twitter:card");return e.jsxs(e.Fragment,{children:[a.url&&e.jsx("meta",{property:"og:url",content:a.url}),e.jsx("title",{children:t.title}),e.jsx("meta",{property:"og:title",content:t.title}),t.description&&e.jsxs(e.Fragment,{children:[e.jsx("meta",{name:"description",content:t.description}),e.jsx("meta",{property:"og:description",content:t.description})]}),e.jsx("meta",{property:"og:type",content:"website"}),e.jsx("meta",{property:"og:site_name",content:h}),p&&e.jsx("meta",{property:"og:image",content:t.socialImageUrl}),e.jsx("script",{type:"application/ld+json",dangerouslySetInnerHTML:{__html:JSON.stringify(o,null,2)}}),t.excludePageFromSearch&&e.jsx("meta",{name:"robots",content:"noindex, nofollow"}),t.custom.map(({property:n,content:j})=>e.jsx("meta",{property:n,content:j},n)),(t.socialImageAssetName!==void 0||t.socialImageUrl!==void 0)&&g===!1&&e.jsx("meta",{property:"twitter:card",content:"summary_large_image"}),e.jsx("link",{rel:"icon",href:i({src:`${r}${z}`})}),v.map(n=>e.jsx("link",{rel:"preload",href:`${r}${n}`,as:"font",crossOrigin:"anonymous"},n)),T.map(n=>e.jsx("link",{rel:"preload",href:`${r}${n}`,as:"image"},n))]})},P=Object.freeze(Object.defineProperty({__proto__:null,Head:A},Symbol.toStringTag,{value:"Module"})),G={isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:{server:!0}}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"/renderer/+onRenderClient.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:y}},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/program/+Page.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:_}},Head:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/program/+Head.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:P}}};export{G as configValuesSerialized};
